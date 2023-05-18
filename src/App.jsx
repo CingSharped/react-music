a6f2b382eb6db685e0887afbe6369211da14a29a
christophersharpe <chrismsharpe123@gmail.com>
2023-05-18 15:53:11 +0100

    404 page aka NotFound page implemented for non existing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,26 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import { NavBar } from './layout'
-import * as Pages from './pages'
-import './App.css'
+import { NavBar } from "./layout";
+import * as Pages from "./pages";
+import "./App.css";
 import { UsernameProvider } from "./contexts";
-// import ArtistDisplay from './components/artistDisplay'
-// import UserForm from './components/userForm'
 
 function App() {
-  // const [username, setUserName] = useState()
-
-  // const handleUsername = (newUser) => {
-  //   setUserName(newUser)
-  // }
-
   return (
     <>
       <UsernameProvider>
         <Routes>
-          <Route path='/' element={<NavBar />}>
-            <Route index element={<Pages.Home />}/>
-            <Route path='/albums' element={<Pages.Albums />}/>
+          <Route path="/" element={<NavBar />}>
+            <Route index element={<Pages.Home />} />
+            <Route path="/albums" element={<Pages.Albums />} />
 
+            <Route path="*" element={<Pages.NotFound />} />
           </Route>
         </Routes>
-
-
-        {/* <h1>{username ? `${username}\'s Music` : 'Music'}</h1>
-        {username ? <></> : <UserForm handleUsername={handleUsername} />}
-        <ArtistDisplay artist='Linkin Park' /> */}
-        
       </UsernameProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
